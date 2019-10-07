5c71da12e9c0893006e00d5312d9a7645a020776
Shin Watanabe <sin9270@gmail.com>
2019-10-07 23:32:24 +0900

    Add dev-server

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,7 +1,13 @@
+const path = require("path");
 const merge = require("webpack-merge");
 const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
   mode: "development",
-  devtool: "eval-source-map"
+  devtool: "eval-source-map",
+  devServer: {
+    contentBase: path.join(__dirname, "public"),
+    host: "0.0.0.0",
+    port: 9000
+  }
 });
